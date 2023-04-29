17fb97d422f6f3e97fa81c5c4b5516a76b4c42c2
Ruben Christoffer Hegland-Antonsen <rubenc@protonmail.com>
2023-04-29 16:50:43 +0200

    Got wall generation working

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -6,17 +6,22 @@ public class LevelGenerator : MonoBehaviour
 {
 
     public GameObject mapSegmentPrefab;
+    public GameObject wallPrefab;
     public GameObject[] tilePrefabs;
 
     public List<GameObject> mapSegments;
 
     void Awake()
     {
-        GenerateMap(10);    
+        GenerateMap(8);    
     }
 
     public Vector3 GetWorldPosition(int xCoordinate, int yCoordinate) {
         return new Vector3(xCoordinate * 60, 0, yCoordinate * 60);
+    }
+
+    public float GetMapSize (int sizeFactor) {
+        return sizeFactor * 60f;
     }
 
     public void GenerateMap (int sizeFactor) {
@@ -29,5 +34,23 @@ public class LevelGenerator : MonoBehaviour
                 }
             }
         }
+
+        float mapSize = GetMapSize(sizeFactor);
+
+        SpawnWall(sizeFactor, new Vector3(mapSize / 2f - 30f, 0f, 0f), Vector3.zero);
+        SpawnWall(sizeFactor, new Vector3(mapSize - 60f - 30f, 0f, mapSize / 2f - 60f), new Vector3(0f, -90f, 0f));
+
+        SpawnWall(sizeFactor, new Vector3(mapSize / 2f - 30f, 0f, mapSize - 60f * 2f), Vector3.zero);
+        SpawnWall(sizeFactor, new Vector3(30f, 0f, mapSize / 2f - 60f), new Vector3(0f, -90f, 0f));
+    }
+
+    private void SpawnWall (int sizeFactor, Vector3 position, Vector3 localEulerAngles) {
+        GameObject wall = Instantiate<GameObject>(wallPrefab, 
+            position,
+            Quaternion.identity
+        );
+
+        wall.transform.localScale = new Vector3(GetMapSize(sizeFactor - 2), 1f, 1f);
+        wall.transform.localEulerAngles = localEulerAngles;
     }
 }
