f6005d2534a5fb869c9394e36a413db34201b70c
Ruben Christoffer Hegland-Antonsen <rubenc@protonmail.com>
2023-04-30 15:33:23 +0200

    Refactored items to avoid code reuse

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -2,44 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Coin : MonoBehaviour
+public class Coin : Item
 {
     
     public int moneyToGain = 5;
-    public float rotateSpeed = 180f;
+    
     public GameState gameState;
-    public LayerMask groundMask;
 
-    private bool activated;
-    private bool flipped;
-    private Transform child;
-
-    void Awake () {
-        child = transform.GetChild(0);
-
-        if (Physics.Raycast(transform.position + Vector3.up * 10f, Vector3.down, out var hit, 100f, groundMask)) {
-            transform.position = hit.point;
-        }
-
-        if (Random.Range(0, 2) == 0) {
-            flipped = true;
-        }
-    }
-
-    void Update () {
-        child.Rotate(new Vector3(0f, 0f, rotateSpeed * Time.deltaTime * (flipped ? -1f : 1f)));
-    }
-
-    void OnTriggerEnter (Collider collider) {
-        if (activated) {
-            return;
-        }
-
+    protected override bool OnPickup(Collider collider)
+    {
         if (collider.GetComponentInParent<PlayerMovement>()) {
             gameState.moneyGained += moneyToGain;
-            activated = true;
-            Destroy(gameObject);
+            return true;
         }
+
+        return false;
     }
 
 }
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public abstract class Item : MonoBehaviour
+{
+
+    public float rotateSpeed = 180f;
+
+    public LayerMask groundMask;
+    
+    public UnityEvent<bool> onPickedUp { get; private set; } = new UnityEvent<bool>();
+
+    private Transform child;
+    private bool flipped;
+    private bool activated;
+
+    protected void Awake () {
+        child = transform.GetChild(0);
+
+        if (Physics.Raycast(transform.position + Vector3.up * 10f, Vector3.down, out var hit, 100f, groundMask)) {
+            transform.position = hit.point;
+        }
+
+        if (Random.Range(0, 2) == 0) {
+            flipped = true;
+        }
+    }
+
+    protected void Update () {
+        child.Rotate(new Vector3(0f, 0f, rotateSpeed * Time.deltaTime * (flipped ? -1f : 1f)));
+    }
+
+    protected void OnTriggerEnter (Collider collider) {
+        if (activated) {
+            return;
+        }
+
+        activated = OnPickup(collider);
+
+        if (activated) {
+            Destroy(gameObject);
+        }
+    }
+
+    protected abstract bool OnPickup (Collider collider);
+
+}
